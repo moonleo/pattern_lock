ef0aee97bdad0df4d5d4f107343a1b0cad67e678
MoonLeo <yanhan_90@126.com>
2016-01-02 22:05:26 +0800

    Initial commit

---
+++ app/src/main/java/per/yh/patternlock/ui/MainActivity.java
@@ -0,0 +1,16 @@
+package per.yh.patternlock.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import per.yh.patternlock.R;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
