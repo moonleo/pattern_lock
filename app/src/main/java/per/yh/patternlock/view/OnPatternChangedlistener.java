dbe8ff34e6a3efe8bbf654a57d9d5201590b62d9
MoonLeo <yanhan_90@126.com>
2016-01-17 23:33:15 +0800

    添加监听器

--- app/src/main/java/per/yh/patternlock/view/OnPatternChangedlistener.java
+++ app/src/main/java/per/yh/patternlock/view/OnPatternChangedlistener.java
@@ -2,6 +2,9 @@ package per.yh.patternlock.view;
 
 /**
  * Created by MoonLeo on 2016/1/16.
+ * 图案变化监听器
  */
-public interface OnPatternChangedlistener {
+public interface OnPatternChangedListener {
+    void patternChanged(String password);
+    void patternStart(boolean start);
 }
