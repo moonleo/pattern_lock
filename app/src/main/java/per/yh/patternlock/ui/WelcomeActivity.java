3d29605319661fa105a0bdeae235032955dae03c
MoonLeo <yanhan_90@126.com>
2016-01-21 23:05:59 +0800

    添加欢迎界面

---
+++ app/src/main/java/per/yh/patternlock/ui/WelcomeActivity.java
@@ -0,0 +1,17 @@
+package per.yh.patternlock.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import per.yh.patternlock.R;
+
+/**
+ * Created by MoonLeo on 2016/1/19.
+ */
+public class WelcomeActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
